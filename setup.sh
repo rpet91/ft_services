5ff646e293ad44d59fef8e9438bcb862fbcdb660
Remco <rpet@student.codam.nl>
2020-10-08 14:37:16 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,19 +1,34 @@
 #!/bin/zsh
+minikube delete
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 minikube addons enable dashboard
-#minikube dashboard &
 
 # Configuring metallb:
 
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb.yaml
 
-# Creating images:
+# Setting up Minikube's Docker:
 
-eval $(minikube docker-env) #eval $(minikube -p minikube docker-env)
+eval $(minikube docker-env)
+
+# Creating an serviceaccount:
+
+kubectl create serviceaccount remco
+kubectl apply -f srcs/serviceaccount.yaml
+
+# Creating nginx:
 
 docker build -t nginx srcs/nginx/
+kubectl apply -f srcs/nginx.yaml
 
-# Creating services:
+# Creating ftps:
 
-kubectl apply -f srcs/
+docker build -t ftps srcs/ftps/
+kubectl apply -f srcs/ftps.yaml
+
+# Creating wordress:
+
+docker build -t wordpress srcs/wordpress/
+kubectl apply -f srcs/wordpress.yaml
