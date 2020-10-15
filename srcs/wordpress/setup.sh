19ce453c815babdd82ffd6a5b4fd86fcacedd00b
Remco <rpet@student.codam.nl>
2020-10-15 14:44:23 +0200

    Add files via upload

---
+++ srcs/wordpress/setup.sh
@@ -0,0 +1,15 @@
+cd /www
+
+APISERVER=https://kubernetes.default.svc
+SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+TOKEN=$(cat ${SERVICEACCOUNT}/token)
+CACERT=${SERVICEACCOUNT}/ca.crt
+URL=$(curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/default/services/wordpress/ 2>/dev/null| jq -r '.status | .loadBalancer | .ingress | .[] | .ip')
+
+wp config create --dbname=wordpress --dbuser=mysql --dbpass=kaas --dbhost=mysql
+wp db create
+wp core install --url=${URL}:5050 --title=wordpress --admin_user=remco --admin_password=kaas --admin_email=rpet@student.codam.nl --skip-email
+wp user create author author@example.com --role=author --user_pass=kaas
+wp user create subscriber subscriber@example.com --role=subscriber --user_pass=kaas
+php-fpm7
+nginx -g "daemon off;"
